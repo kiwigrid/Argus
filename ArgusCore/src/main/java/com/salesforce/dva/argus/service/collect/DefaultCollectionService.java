4975aebd4704abfa1333c6b807e83e16ce321646
Axel Köhler <axel.koehler@kiwigrid.com>
2017-05-17 15:00:38 +0200

    ALERT-194 commit schema workaround

--- ArgusCore/src/main/java/com/salesforce/dva/argus/service/collect/DefaultCollectionService.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/collect/DefaultCollectionService.java
@@ -187,6 +187,8 @@ public class DefaultCollectionService extends DefaultJPAService implements Colle
         if (!dequeued.isEmpty()) {
             _tsdbService.putMetrics(dequeued);
             _logger.debug("Committed {} metrics.", dequeued.size());
+            _schemaService.put(dequeued);
+            _logger.debug("Committed {} metrics for schema records creation.", dequeued.size());
         }
         return dequeued;
     }
